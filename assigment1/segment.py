b2148c224f8fbb69d88a8cb31301d16ed6e22996
Ondrej Vesely <ondrej-vesely@seznam.cz>
2020-09-03 14:56:14 +0200

    Initial commit

---
+++ assigment1/segment.py
@@ -0,0 +1,26 @@
+# GEO1000 - Assignment 1
+# Authors: Ondrej Vesely,
+# Studentnumbers: 5162130,
+
+import math
+
+def distance(x1, y1, x2, y2):
+    pass
+
+
+def heron(a, b, c):
+    pass
+
+
+def angle(a, b, c):
+    pass
+
+
+def segment_point_dist(s1x, s1y, s2x, s2y, px, py):
+    pass
+
+
+print(segment_point_dist(0, 0, 10, 0, 5, 10))
+print(segment_point_dist(0, 0, 10, 0, 20, 0))
+print(segment_point_dist(0, 0, 10, 0, -10, -10))
+print(segment_point_dist(0, 0, 10, 10, 0, 10))
---
+++ assigment1/solve.py
@@ -0,0 +1,18 @@
+# GEO1000 - Assignment 1
+# Authors: Ondrej Vesely,
+# Studentnumbers: 5162130,
+
+from math import sqrt
+
+
+def abc(a, b, c):
+    pass
+
+
+def discriminant(a, b, c):
+    pass
+
+
+abc(2.0, 0.0, 0.0)
+abc(1.0, 3.0, 2.0)
+abc(3.0, 4.5, 9.0)
