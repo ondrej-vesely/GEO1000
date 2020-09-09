844cb37059ec8a473a72638f319099ef7e34c520
Ondrej Vesely <ondrej-vesely@seznam.cz>
2020-09-09 11:53:43 +0200

    second task

--- assigment1/segment.py
+++ assigment1/segment.py
@@ -4,22 +4,42 @@
 
 import math
 
+
 def distance(x1, y1, x2, y2):
-    pass
+    """Calculate the distance between two points."""
+    return math.sqrt((x2-x1)**2 + (y2-y1)**2)
 
 
 def heron(a, b, c):
-    pass
+    """Calculate the area of a triangle."""
+    s = (a+b+c) / 2
+    return math.sqrt(s * (s-a) * (s-b) * (s-c))
 
 
 def angle(a, b, c):
-    pass
+    """Calculate the angle between sides a, b of triangle."""
+    return math.acos((a**2 + b**2 - c**2) / (2*a*b))
 
 
 def segment_point_dist(s1x, s1y, s2x, s2y, px, py):
-    pass
+    """Calculate distance of a point to a line segment."""
+    # calculate the triangle sides
+    a = distance(s2x, s2y, px, py)
+    b = distance(px, py, s1x, s1y)
+    c = distance(s1x, s1y, s2x, s2y)
 
+    # check alpha angle
+    if angle(b, c, a) >= math.pi/2:
+        return distance(s1x, s1y, px, py)
+    # check beta angle
+    elif angle(c, a, b) >= math.pi/2:
+        return distance(s2x, s2y, px, py)
+    # if on line do the triangle height
+    else:
+        area = heron(a, b, c)
+        return area/c*2
 
+# test
 print(segment_point_dist(0, 0, 10, 0, 5, 10))
 print(segment_point_dist(0, 0, 10, 0, 20, 0))
 print(segment_point_dist(0, 0, 10, 0, -10, -10))
