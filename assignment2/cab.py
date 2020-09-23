c7af210d4d02e66cfbb46fee5583d8149ac95bcc
Ondrej Vesely <ondrej-vesely@seznam.cz>
2020-09-23 09:40:35 +0200

    cab

--- assignment2/cab.py
+++ assignment2/cab.py
@@ -1,10 +1,17 @@
 # GEO1000 - Assignment 2
-# Authors:
-# Studentnumbers:
+# Authors: Ondrej Vesely, Giorgos Triantafyllou
+# Studentnumbers: 5162130, 5381738
 
 
 def wiggle(start, end, moves):
-    pass
+    length = abs(end - start)
+
+    if length > moves:
+        return 0
+    if length == moves:
+        return 1
+    else:
+        return 1 + ((moves-length)//2)*(length+1)
 
 
 if __name__ == "__main__":
