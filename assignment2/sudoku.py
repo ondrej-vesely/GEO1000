395b8265c51231508b718e6559ea1d23f1bc92b2
Ondrej Vesely <ondrej-vesely@seznam.cz>
2020-09-23 11:18:51 +0200

    sudoku

--- assignment2/sudoku.py
+++ assignment2/sudoku.py
@@ -1,18 +1,59 @@
 # GEO1000 - Assignment 2
-# Authors:
-# Studentnumbers:
+# Authors: Ondrej Vesely, Giorgos Triantafyllou
+# Studentnumbers: 5162130, 5381738
+
+
+_bitmasks = [
+    # rows
+    '1111000000000000',
+    '0000111100000000',
+    '0000000011110000',
+    '0000000000001111',
+    # columns
+    '1000'*4,
+    '0100'*4,
+    '0010'*4,
+    '0001'*4,
+    # cells
+    '1100110000000000',
+    '0011001100000000',
+    '0000000011001100',
+    '0000000000110011'
+]
 
 
 def parse(sudoku):
-    pass
+    if len(sudoku) != 16:
+        return None
+    return [[n for n, bit in zip(sudoku, mask) if int(bit)] for mask in _bitmasks]
 
 
-def is_valid(stucture):
-    pass
+def is_valid(structure):
+    test = range(1,4)
+    for chunk in structure:
+        for number in test:
+            if not number in chunk:
+                return False
+    return True
 
 
 def main():
-    pass
+    sudoku = input('Your sudoku for validation, or "quit" to exit:  ')
+
+    if sudoku == 'quit':
+        pass
+
+    else:
+        chunks = parse(sudoku)
+        if chunks and sudoku.isdigit:
+            if is_valid(chunks):
+                print('This sudoku is *VALID*')
+            else:
+                print('This sudoku is *INVALID*')
+        else:
+            print('Input not understood.')
+
+        main()
 
 
 if __name__ == "__main__":
