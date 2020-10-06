efcb731ea99aac0d3d2d35b84a93a2e83483d125
Giorgos Triantafyllou <geo30fyllou@gmail.com>
2020-10-06 21:17:25 +0200

    Assignement 3

---
+++ assignment3/hunt.py
@@ -0,0 +1,24 @@
+# GEO1000 - Assignment 3
+# Authors: Ondrej Vesely, Giorgos Triantafyllou
+# Studentnumbers: 5162130, 5381738
+
+def read_grid(filenm):
+    """
+    """
+    pass
+
+
+def visit(table, steps_allowed, path):
+    """
+    """
+    pass
+
+
+def hunt(filenm, max_steps):
+    """
+    """
+    pass
+
+
+if __name__ == "__main__":
+    print(hunt('finite.txt', 20))
